0281ec2a6ab142fc1090890cdeeea575d4891cab
Arien Malec <arien.malec@nhindirect.org>
2010-05-16 12:07:06 -0700

    Finshed off interactive User management forms

--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -8,6 +8,7 @@ ActionController::Routing::Routes.draw do |map|
   
   map.resource :user_session
   map.root :controller => 'user_sessons', :action => 'new'
+  map.resource :users
 
   # The priority is based upon order of creation: first created -> highest priority.
 
