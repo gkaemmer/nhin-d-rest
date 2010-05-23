9684aa0515abf35b5d667f2de92999b4895b7cfc
Chris Moyer <chris.moyer@gmail.com>
2010-05-23 15:25:25 -0400

    Major set of changes.
    Refactored some of the Controller code.
    Cleaned up resource leak in RestClient so second connection works.
    Added PUT STATUS tunnelling from destination HISP to source HISP.
    Changed GET MESSAGE so that sent messages can be retrieved.
    Changed GET STATUS so that the status of sent messages can be retrieved.

--- spring-maven-poc/src/main/java/org/nhindirect/platform/rest/NhinDirect10Controller.java
+++ spring-maven-poc/src/main/java/org/nhindirect/platform/rest/NhinDirect10Controller.java
@@ -1,20 +1,20 @@
 package org.nhindirect.platform.rest;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.util.List;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.nhindirect.platform.HealthAddress;
 import org.nhindirect.platform.Message;
 import org.nhindirect.platform.MessageService;
 import org.nhindirect.platform.MessageServiceException;
 import org.nhindirect.platform.MessageStatus;
 import org.nhindirect.platform.MessageStoreException;
-import org.nhindirect.platform.basic.AbstractUserAwareClass;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -23,23 +23,23 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Controller for NHIN Direct REST API as defined here: http://nhindirect.org/REST+Implementation
  * 
- * This is not intended as a secure, reliable, or complete implementation. It's
- * intended as a proof of concept on using Spring MVC 3.0 for the REST
- * implementation.
+ * This is not intended as a secure, reliable, or complete implementation. It's intended as a proof
+ * of concept on using Spring MVC 3.0 for the REST implementation.
  * 
  * 
  */
 @Controller
 @RequestMapping("/v1/{healthDomain}/{healthEndpoint}/messages")
-public class NhinDirect10Controller extends AbstractUserAwareClass {
+public class NhinDirect10Controller extends AbstractNhinDirectController {
 
     @Autowired
     protected MessageService messageService;
+
+    private Log log = LogFactory.getLog(NhinDirect10Controller.class);
 
     /**
      * Get messages addressed to a specified health address.
@@ -69,8 +69,6 @@ public class NhinDirect10Controller extends AbstractUserAwareClass {
                             @PathVariable("healthEndpoint") String healthEndpoint, @RequestBody String rawMessage)
             throws MessageStoreException, MessageServiceException {
 
-        System.out.println("*** USER Roles: " + getUser().getAuthorities());
-        
         HealthAddress address = new HealthAddress(healthDomain, healthEndpoint);
         Message message = messageService.handleMessage(address, rawMessage);
         response.setHeader("Location", request.getRequestURL().toString() + "/" + message.getMessageId());
@@ -101,89 +99,56 @@ public class NhinDirect10Controller extends AbstractUserAwareClass {
      */
     @PreAuthorize("hasRole('ROLE_EDGE')")
     @RequestMapping(value = "/{messageId}/status", method = RequestMethod.GET)
-    @ResponseBody
-    public String getMessageStatus(@PathVariable("healthDomain") String healthDomain,
-                                   @PathVariable("healthEndpoint") String healthEndpoint,
-                                   @PathVariable("messageId") String messageId) throws MessageStoreException,
-            MessageServiceException {
+    public void getMessageStatus(HttpServletResponse response, @PathVariable("healthDomain") String healthDomain,
+                                 @PathVariable("healthEndpoint") String healthEndpoint,
+                                 @PathVariable("messageId") String messageId) throws MessageStoreException,
+            MessageServiceException, IOException {
 
         HealthAddress address = new HealthAddress(healthDomain, healthEndpoint);
         Message message = messageService.getMessage(address, UUID.fromString(messageId));
 
-        return message.getStatus().toString();
+        if (message == null) {
+            sendSimpleResponse(response, "Message " + messageId + " not found", 404);
+        } else {
+            sendSimpleResponse(response, message.getStatus().toString());
+        }
+
     }
 
     /**
-     * Set the status of a specific message sent to the specified health address
+     * Set the status of a specific message sent to the specified health address. If it's the edge
+     * recipient changing the status, then store the change and PUT that status back to the sender
+     * HISP. If if the's destination HISP pushing the status back, then store the status change.
      */
     @PreAuthorize("hasRole('ROLE_EDGE') or hasRole('ROLE_HISP')")
     @RequestMapping(value = "/{messageId}/status", method = RequestMethod.PUT)
-    @ResponseBody
-    public String setMessageStatus(@PathVariable("healthDomain") String healthDomain,
-                                   @PathVariable("healthEndpoint") String healthEndpoint,
-                                   @PathVariable("messageId") String messageId, @RequestBody String status)
-            throws NumberFormatException, MessageStoreException, MessageServiceException {
+    public void setMessageStatus(HttpServletResponse response, @PathVariable("healthDomain") String healthDomain,
+                                 @PathVariable("healthEndpoint") String healthEndpoint,
+                                 @PathVariable("messageId") String messageId, @RequestBody String status)
+            throws NumberFormatException, MessageStoreException, MessageServiceException, IOException {
 
         HealthAddress address = new HealthAddress(healthDomain, healthEndpoint);
         messageService.setMessageStatus(address, UUID.fromString(messageId), MessageStatus
                 .valueOf(status.toUpperCase()));
 
-        Message message = messageService.getMessage(address, UUID.fromString(messageId));
-
-        return message.getStatus().toString();
+        sendSimpleResponse(response, status);
     }
 
     /**
-     * Process MessageServiceException as an HTTP 400 error and return simple
-     * explanation to client.
+     * Process MessageServiceException as an HTTP 400 error and return simple explanation to client.
      */
     @ExceptionHandler(MessageServiceException.class)
     public void handleMessageServiceException(Exception e, HttpServletResponse response) throws IOException {
+        log.error(e);
         sendSimpleResponse(response, e.getMessage(), 400);
     }
 
     /**
-     * Process MessageStoreException as an HTTP 500 error and return simple
-     * explanation to client.
+     * Process MessageStoreException as an HTTP 500 error and return simple explanation to client.
      */
     @ExceptionHandler(MessageStoreException.class)
     public void handleMessageStoreException(Exception e, HttpServletResponse response) throws IOException {
+        log.error(e);
         sendSimpleResponse(response, e.getMessage(), 500);
-    }
-
-    /**
-     * Sends a simple HTTP response with a specified content type.
-     */
-    private void sendSimpleResponse(HttpServletResponse response, String message, String contentType)
-            throws IOException {
-        sendSimpleResponse(response, message, contentType, -1);
-    }
-
-    /**
-     * Sends a simple HTTP response with a specified status code.
-     */
-    private void sendSimpleResponse(HttpServletResponse response, String message, int status) throws IOException {
-        sendSimpleResponse(response, message, null, status);
-    }
-    
-    /**
-     * Sends a simple HTTP response with a specified content type and and status
-     * code.
-     */
-    private void sendSimpleResponse(HttpServletResponse response, String message, String contentType, int status)
-            throws IOException {
-
-        if (status > -1) {
-            response.setStatus(status);
-        }
-
-        if (contentType != null) {
-            response.setContentType(contentType);
-        }
-
-        PrintWriter out = new PrintWriter(response.getWriter());
-
-        out.print(message);
-        out.close();
     }
 }
